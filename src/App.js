a468efa7cf44cf18fdd26def8677ff99239aa5cb
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-21 16:25:03 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
