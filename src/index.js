a5275dad926dd04edcf1c4e5e43a1ccded21e26b
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-21 16:26:04 +0200

    Wrapp the context provider to app component

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <GithubProvider>
+      <App />
+    </GithubProvider>
+  </React.StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
