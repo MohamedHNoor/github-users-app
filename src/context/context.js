38ba1e33122e3df2e05b5889befb59f95935bd33
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-23 16:04:59 +0200

    Fetch mock data

--- src/context/context.js
+++ src/context/context.js
@@ -9,8 +9,13 @@ const rootUrl = 'https://api.github.com';
 const GithubContext = React.createContext();
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
   return (
-    <GithubContext.Provider value="hello">{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
