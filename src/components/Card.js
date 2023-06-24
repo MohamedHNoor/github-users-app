d57ce46e9573772bf35a746c9f252752fa1878e1
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-24 20:02:53 +0200

    Add user info to the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,41 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username,
+  } = githubUser;
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || name}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p className="bio">{bio}</p>
+      <div className="links">
+        <p>
+          <MdBusiness /> {company}
+        </p>
+        <p>
+          <MdLocationOn /> {location || 'earth'}
+        </p>
+        <a href={blog || 'none'}>
+          <MdLink /> {blog}
+        </a>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
