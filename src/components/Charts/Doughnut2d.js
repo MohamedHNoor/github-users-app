1b0df7926a9da3a3f750ac6dfdab120c1e89582f
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-26 21:14:40 +0200

    Add doughnout2d chart

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,42 @@
+// STEP 1 - Include Dependencies
+// Include react
 import React from 'react';
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
+// Include the react-fusioncharts component
+import ReactFC from 'react-fusioncharts';
+
+// Include the fusioncharts library
+import FusionCharts from 'fusioncharts';
+
+// Include the chart type
+import Chart from 'fusioncharts/fusioncharts.charts';
+
+// Include the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.candy';
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: 'doughnut2d', // The chart type
+    width: '100%', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: 'Stars Per Language',
+        theme: 'candy',
+        decimals: 0,
+        doughnutRadius: '45%',
+        showPercentValues: 0,
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Doughnut2d;
+export default ChartComponent;
