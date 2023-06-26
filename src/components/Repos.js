75f54c03276092ad7a0e798133f6756f2a1705fa
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-26 20:22:04 +0200

    Destructure most used language in the repos and rendered on browser

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,8 +4,46 @@ import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+  languages = Object.values(languages)
+    .sort((a, b) => b.value - a.value)
+    .slice(0, 5);
 
-  return <h2>repos component</h2>;
+  const chartData = [
+    {
+      label: 'HTML',
+      value: '14',
+    },
+    {
+      label: 'CSS',
+      value: '160',
+    },
+    {
+      label: 'JavaScript',
+      value: '80',
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <Pie3D data={languages} />
+        {/*<ExampleChart data={chartData} />*/}
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
