d52da2488dae7ff023fb1aa60174d9a460519547
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-29 02:10:48 +0200

    Add functionality to search bar component

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,14 +4,23 @@ import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
   const [user, setUser] = React.useState('');
+  const { requests, error, searchGithubUser, isLoading } =
+    React.useContext(GithubContext);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (user) {
+      searchGithubUser(user);
     }
   };
   return (
     <section className="section">
       <Wrapper className="section-center">
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <MdSearch />
@@ -21,10 +30,13 @@ const Search = () => {
               value={user}
               onChange={(e) => setUser(e.target.value)}
             />
-            <button type="submit">Search</button>
+
+            {requests > 0 && !isLoading && (
+              <button type="submit">Search</button>
+            )}
           </div>
         </form>
-        <h3>Requests: 60/60</h3>
+        <h3>Requests: {requests}/60</h3>
       </Wrapper>
     </section>
   );
