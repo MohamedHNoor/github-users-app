00330a61b2cdab618e9085ffc473c06f1148dec0
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-29 02:11:55 +0200

    Add functionality to fetch user, repos and and followers from github API

--- src/context/context.js
+++ src/context/context.js
@@ -12,8 +12,80 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+
+  // request loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: '' });
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((error) => {
+      console.log(error);
+    });
+    if (response) {
+      setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then((results) => {
+          const [repos, followers] = results;
+          const status = 'fulfilled';
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      toggleError(true, 'There is no user with this username');
+    }
+    checkRequest();
+    setIsLoading(false);
+  };
+
+  const checkRequest = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, 'Sorry, you have exceeded your hourly rate limit!');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const toggleError = (show = false, msg = '') => {
+    setError({ show, msg });
+  };
+
+  useEffect(checkRequest, []);
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
