35fc769954712aaf0fdb941cb0fc496f68b9dd96
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-06-30 16:43:00 +0200

    Add auth0Provider to the app

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+
+//
+//
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-zwtvqsniverv0kuz.us.auth0.com"
+      clientId="r3gDVLcJKhptnMfV57F00tLMsXqZB0LR"
+      redirect_uri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
 );
 
