ab822c354c87a44f055cea1ec9dbb046c6576241
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-07-01 22:22:12 +0200

    Add login and logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,26 @@ import { useAuth0 } from '@auth0/auth0-react';
 const Navbar = () => {
   const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
     useAuth0();
+  const isUser = isAuthenticated && user;
   return (
     <Wrapper>
-      <button onClick={loginWithRedirect}>Login</button>
+      {isUser && user.picture && <img src={user.picture} alt={user.name} />}
+      {isUser && user.name && (
+        <h4>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
+        </h4>
+      )}
+      {isUser ? (
+        <button
+          onClick={() => {
+            logout({ returnTo: window.location.origin });
+          }}
+        >
+          logout
+        </button>
+      ) : (
+        <button onClick={loginWithRedirect}>login</button>
+      )}
     </Wrapper>
   );
 };
