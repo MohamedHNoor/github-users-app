cc32e953fc921dbf3552d3b9586511998f0084df
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-07-02 17:37:04 +0200

    Add private routes to the app

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const PrivateRoute = () => {
-  return <h2>private route component</h2>;
+const PrivateRoute = ({ children }) => {
+  const { isAuthenticated, user } = useAuth0();
+  const isUser = isAuthenticated && user;
+
+  if (!isUser) {
+    return <Navigate to="/login" />;
+  }
+  return children;
 };
 export default PrivateRoute;
