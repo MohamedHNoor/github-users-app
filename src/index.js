450ca7d7c50c80d60b8d338b29663b116a2ba8ac
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-07-16 19:58:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-//
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
