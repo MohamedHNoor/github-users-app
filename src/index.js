0aac266f529a422d54c1e8bd88d7005d9bc1b413
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-07-16 19:58:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,6 +23,6 @@ root.render(
 );
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+
 
 serviceWorker.unregister();
