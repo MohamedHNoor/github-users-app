c34f5ffd9e7345838af4a3b189346785ddd25951
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-07-30 16:16:34 +0200

    Update Doughnut2d.js

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -12,7 +12,7 @@ const ChartComponent = ({ data }) => {
     type: 'doughnut2d',
     width: '100%',
     height: '400',
-    dataFormat: 'json', // Data type
+    dataFormat: 'json', 
     dataSource: {
       // Chart Configuration
       chart: {
